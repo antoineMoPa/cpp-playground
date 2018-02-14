e1cac725edbb7c54a1e3b11feca519bf24f2467f
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2018-02-14 17:59:35 -0500

    Compute averages of many letters

--- ann/png_read.cpp
+++ ann/png_read.cpp
@@ -7,15 +7,35 @@ namespace fs = std::experimental::filesystem;
 using namespace cimg_library;
 
 int main(int argc, char ** argv){
-
-    for(auto& p: fs::directory_iterator("set/fonts")){
-        std::string path = p.path().string() + "/a.png";
+    CImg<unsigned char> ones(100,100,1,3,255);
+    
+    for(char letter = 'a'; letter <= 'z'; letter++){
+        CImg<int> letter_tot(100,100,1,3,0);
+        CImg<unsigned char> letter_avg(100,100,1,3,0);
+        int image_count = 0;
         
-        if(fs::exists(path)){
-            std::cout << "Opening: " << path << std::endl;
-            CImg<float> image(path.c_str());
-            CImgDisplay main_disp(image);
+        for(auto& p: fs::directory_iterator("set/fonts")){
+            std::string path = p.path().string() + "/"+letter+".png";
+            
+            if(fs::exists(path)){
+                std::cout << "Opening: " << path << std::endl;
+                CImg<unsigned char> current_image(path.c_str());
+                
+                current_image.resize(100,100,1,3,0, 1, 0.5, 0.5);
+                current_image = ones - current_image;
+                
+                letter_tot += current_image;
+                image_count++;
+                
+                //CImgDisplay display(letter_tot.resize(100, 100));
+                //pause();
+                //break;
+            }
         }
+        
+        letter_avg = letter_tot / image_count;
+        letter_avg.save_png((std::string("results/") + letter + "_avg.png").c_str());
+        
     }
     
     return 0;
