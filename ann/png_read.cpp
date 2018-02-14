b1999224059735beddf342e4789ba5b0fb29365c
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2018-02-14 18:04:38 -0500

    go back to 15 pixel dim

--- ann/png_read.cpp
+++ ann/png_read.cpp
@@ -7,11 +7,11 @@ namespace fs = std::experimental::filesystem;
 using namespace cimg_library;
 
 int main(int argc, char ** argv){
-    CImg<unsigned char> ones(100,100,1,3,255);
+    CImg<unsigned char> ones(15,15,1,3,255);
     
     for(char letter = 'a'; letter <= 'z'; letter++){
-        CImg<int> letter_tot(100,100,1,3,0);
-        CImg<unsigned char> letter_avg(100,100,1,3,0);
+        CImg<int> letter_tot(15,15,1,3,0);
+        CImg<unsigned char> letter_avg(15,15,1,3,0);
         int image_count = 0;
         
         for(auto& p: fs::directory_iterator("set/fonts")){
@@ -21,13 +21,13 @@ int main(int argc, char ** argv){
                 std::cout << "Opening: " << path << std::endl;
                 CImg<unsigned char> current_image(path.c_str());
                 
-                current_image.resize(100,100,1,3,0, 1, 0.5, 0.5);
+                current_image.resize(15,15,1,3,0, 1, 0.5, 0.5);
                 current_image = ones - current_image;
                 
                 letter_tot += current_image;
                 image_count++;
                 
-                //CImgDisplay display(letter_tot.resize(100, 100));
+                //CImgDisplay display(letter_tot.resize(15, 15));
                 //pause();
                 //break;
             }
