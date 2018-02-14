ca7c9b8b5bc37d486a706022ee65287d85f3af2c
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2018-02-14 11:00:56 -0500

    list all font folders

--- ann/png_read.cpp
+++ ann/png_read.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
 #include "CImg.h"
+#include <experimental/filesystem>
+#include <string>
 
+namespace fs = std::experimental::filesystem;
 using namespace cimg_library;
 
 int main(int argc, char ** argv){
-    CImg<float> image("set/a.png");
-    CImgDisplay main_disp(image);
-    
-    pause();
+
+    for(auto& p: fs::directory_iterator("set/fonts")){
+        std::string path = p.path().string() + "/a.png";
+        
+        if(fs::exists(path)){
+            std::cout << "Opening: " << path << std::endl;
+            CImg<float> image(path.c_str());
+            CImgDisplay main_disp(image);
+        }
+    }
     
     return 0;
 }
